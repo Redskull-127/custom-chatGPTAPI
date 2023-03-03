4ca5f92dfff253f7bc9cb4ebf75d37473ca02b5f
Meer Tarbani <76103614+Redskull-127@users.noreply.github.com>
2023-03-03 22:30:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(express.json());
 app.use("/", Router);
 
+app.get("/test", (req, res) => {
+  try{
+     res.status(200).json({message: "Healthy"})
+  } catch(err) {
+     res.status(500).json({message: err.message})
+  }
+
+
+)
+
 const port = 8080;
 app.listen(port, (err) => {
   if (err) {
